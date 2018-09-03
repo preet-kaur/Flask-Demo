ff705173077dce76fe198533e8571dfc3b000441
preet-kaur <gurpreet.saimy@gmail.com>
2018-09-03 22:49:56 +0530

    Basic App Done!

---
+++ basicApp/main.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)  # app is Flask object __name__: help determine root path
+
+
+@app.route('/')
+def index():
+    return 'This is the homepage'
+
+
+if __name__ == "__main__":  # makes sure that app.run(..) is executed only when this file is called directly
+    app.run(debug=True)   #Start this app/web server
+    #Run app only if it is main file
