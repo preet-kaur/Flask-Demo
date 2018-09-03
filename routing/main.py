5dccd7ef841e189c4a09657709237f97cfde7c06
preet-kaur <gurpreet.saimy@gmail.com>
2018-09-03 23:36:06 +0530

    Passing Integers in url

--- routing/main.py
+++ routing/main.py
@@ -19,5 +19,11 @@ def profile(username):
     return "Hello %s" % username
 
 
+# Passing Integers in URL; need to specify datatype for integers in url
+@app.route('/post/<int:post_id>')
+def post(post_id):
+    return "POST ID is %s" % post_id
+
+
 if __name__ == '__main__':
     app.run()
